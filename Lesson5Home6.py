e09cb48ecdadc8df7d786179515f0d789cdd969e
Olha Rodkina <ovrodkina@gmail.com>
2022-01-18 22:38:20 +0200

    Initial commit

---
+++ Lesson5Home6.py
@@ -0,0 +1,22 @@
+import random
+
+heights = [random.randint(150, 200) for _ in range(10)]
+heights.sort()
+petya = 175
+numerated = enumerate(heights)
+
+for i, h in numerated:
+    if petya < heights[0]:
+        heights.insert(0, petya)
+        break
+    if petya > heights[-1]:
+        heights.append(petya)
+        break
+    if petya > h:
+        continue
+    else:
+        heights.insert(i, petya)
+        heights.sort(reverse=True)
+        break
+
+print(heights)
