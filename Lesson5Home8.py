7a6468aadb1fbab347f984769cf8e8e65bd4b4ff
Olha Rodkina <ovrodkina@gmail.com>
2022-01-19 22:44:52 +0200

    Initial commit

--- Lesson5Home8.py
+++ Lesson5Home8.py
@@ -4,14 +4,32 @@
 так и во втором.
 Множество использовать нельзя"""
 
+m = [6, 8, 10, 78, 68]
+n = [8, 9, 23, 78, 64]
+"""p = 0
+k = []
+
+for i in m:
+    if i != n[p]:
+        p += 1
+        k.append(i)
+print(k)
+print(len(k))"""
+
+for i in m:
+    for p in n:
+        if i == p:
+            m.remove(i)
+            n.remove(p)
+t = m + n
+print(t)
+print(len(t))
+
+
+"""
 import random
 
 a = [random.randint(15, 150) for _ in range(10)]
 b = [random.randint(15, 150) for _ in range(10)]
-c = []
-print(a, b)
-for i in a:
-    if a[i] != b[0]:
-        c.append(a[i])
-print(len(c))
+print(len([i for i in [*a, *b] if [*a, *b].count(i) == 1]))"""
 
